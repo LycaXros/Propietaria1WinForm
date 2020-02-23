c2a8fae8e5b52a789504e119d1422133ce92bc87
Jesus Francisco Dicent Aguero <jesusdicent@gmail.com>
2020-02-23 02:48:18 -0400

    Seed Data Departamentos

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace Data.Migrations
 {
+    using Data.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.IO;
@@ -21,12 +23,22 @@
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
 
-            //string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            //UriBuilder uri = new UriBuilder(codeBase);
-            //string path = Uri.UnescapeDataString(uri.Path);
-            //var baseDir = Path.GetDirectoryName(path) + "\\Migrations\\EmployeePuestoView.sql";
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var baseDir = Path.GetDirectoryName(path) + "\\Migrations\\EmployeePuestoView.sql";
 
-            //context.Database.ExecuteSqlCommand(File.ReadAllText(baseDir));
+            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDir));
+            IList<Departamentos> dList = new List<Departamentos>();
+            dList.Add(new Departamentos() { Nombre = "Tecnologias de la Informacion", Estado = EstadoPersistencia.Activo });
+            dList.Add(new Departamentos() { Nombre = "Ventas", Estado = EstadoPersistencia.Activo });
+            dList.Add(new Departamentos() { Nombre = "Soporte Tecnico", Estado = EstadoPersistencia.Inactivo });
+
+            context.Departamentos.AddRange(dList);
+
+
+            base.Seed(context);
+
         }
     }
 }
